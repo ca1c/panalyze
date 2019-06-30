7cc902c9f48cc33be87a929f19c5f8d890b63af4
talonbragg <w1tchhunt@protonmail.com>
2019-06-29 22:03:35 -0700

    remove a console.log that was used for error handling; code formatting

--- bin/panalyze.js
+++ bin/panalyze.js
@@ -18,7 +18,7 @@ require('yargs')
             describe: 'ip to be scanned'
         })
         yargs.positional('options', {
-            type:'string',
+            type: 'string',
             describe: 'type of scan'
         })
         yargs.positional('rangeFN', {
@@ -29,92 +29,84 @@ require('yargs')
             type: 'string',
             describe: 'second number of port range (optional but must have both numbers if using)',
         })
-    }, function (argv) {
-            let scannedip;
-            if(!argv.ip) {
-                console.log(`${logSymbols.warning} ${chalk.yellow('Please provide the ip of the host you would like to scan')}`)
-            }
-            else if(argv.ip && argv.ip !== 'localpublic') {
-                scannedip = argv.ip;
+    }, function(argv) {
+        let scannedip;
+        if (!argv.ip) {
+            console.log(`${logSymbols.warning} ${chalk.yellow('Please provide the ip of the host you would like to scan')}`)
+        } else if (argv.ip && argv.ip !== 'localpublic') {
+            scannedip = argv.ip;
+            scan();
+        } else if (argv.ip == 'localpublic') {
+            getIP((err, ip) => {
+                if (err) {
+                    throw err;
+                }
+                scannedip = ip;
                 scan();
-            }
-            else if(argv.ip == 'localpublic') {
-                getIP((err, ip) => {
-                    if(err) {
-                        throw err;
-                    }
-                    scannedip = ip;
-                    scan();
-                });
-            }
-            if (!argv.options) {
-                console.log(`${logSymbols.warning} ${chalk.yellow('Please provide the type of scan you would like to start')}`);
-            }
-            function scan() {
-                    if(argv.options == 'q') {
-                        for(let i = 0; i < portOptions.quickScanArray.length; i++) {
-                            connectionTester.test(
-                                scannedip,
-                                portOptions.quickScanArray[i],
-                                1000,
-                                (err, output) => {
-                                    console.log(chalk.red(scannedip));
-                                    if(err) throw err;
-                                    else {
-                                        if(output.success == false) {
-                                            console.log(`${logSymbols.error} ${chalk.blue('Port:')} ${chalk.green(portOptions.quickScanArray[i])} ${chalk.blue('IP:')} ${chalk.green(scannedip)}`);
-                                        }
-                                        else if(output.success == true) {
-                                            console.log(`${logSymbols.success} ${chalk.blue('Port:')} ${chalk.green(portOptions.quickScanArray[i])} ${chalk.blue('IP:')} ${chalk.green(scannedip)}`);
-                                        }
-                                    }
+            });
+        }
+        if (!argv.options) {
+            console.log(`${logSymbols.warning} ${chalk.yellow('Please provide the type of scan you would like to start')}`);
+        }
+
+        function scan() {
+            if (argv.options == 'q') {
+                for (let i = 0; i < portOptions.quickScanArray.length; i++) {
+                    connectionTester.test(
+                        scannedip,
+                        portOptions.quickScanArray[i],
+                        1000,
+                        (err, output) => {
+                            if (err) throw err;
+                            else {
+                                if (output.success == false) {
+                                    console.log(`${logSymbols.error} ${chalk.blue('Port:')} ${chalk.green(portOptions.quickScanArray[i])} ${chalk.blue('IP:')} ${chalk.green(scannedip)}`);
+                                } else if (output.success == true) {
+                                    console.log(`${logSymbols.success} ${chalk.blue('Port:')} ${chalk.green(portOptions.quickScanArray[i])} ${chalk.blue('IP:')} ${chalk.green(scannedip)}`);
                                 }
-                            )
-                        }
-                    }
-                    if(argv.options == 'r') {
-                        if(argv.options == 'r' && argv.rangeFN > 0 && argv.rangeSN < 65535) {
-                            for(let i = argv.rangeFN; i - 1 < argv.rangeSN; i++) {
-                                connectionTester.test(
-                                    scannedip,
-                                    i,
-                                    1000,
-                                    (err, output) => {
-                                        if(err) throw err;
-                                        else {
-                                            if(output.success == false) {
-                                                console.log(`${logSymbols.error} ${chalk.blue('Port:')} ${chalk.green(i)} ${chalk.blue('IP:')} ${chalk.green(scannedip)}`);
-                                            }
-                                            else if(output.success == true) {
-                                                console.log(`${logSymbols.success} ${chalk.blue('Port:')} ${chalk.green(i)} ${chalk.blue('IP:')} ${chalk.green(scannedip)}`);
-                                            }
-                                        }
-                                    }
-                                )
                             }
                         }
-                        
-                        else if(argv.options == 'r' && !argv.rangeFN || !argv.rangeSN) {
-                            console.log(`${logSymbols.warning} ${chalk.yellow('Please either provide the first, last or both range numbers')}`);
-                        }
-                        else if(argv.options == 'r' && argv.rangeFN <= 0 || argv.rangeSN > 65535) {
-                            console.log(`${logSymbols.warning} ${chalk.yellow('The first range number must be greater than 1 and the second number must be less than 65535')}`);
-                        }
-                        else {
-                            console.log(`${logSymbols.warning} ${chalk.yellow('Please give a valid scan type')}`)
-                        }
-                    }
+                    )
                 }
+            }
+            if (argv.options == 'r') {
+                if (argv.options == 'r' && argv.rangeFN > 0 && argv.rangeSN < 65535) {
+                    for (let i = argv.rangeFN; i - 1 < argv.rangeSN; i++) {
+                        connectionTester.test(
+                            scannedip,
+                            i,
+                            1000,
+                            (err, output) => {
+                                if (err) throw err;
+                                else {
+                                    if (output.success == false) {
+                                        console.log(`${logSymbols.error} ${chalk.blue('Port:')} ${chalk.green(i)} ${chalk.blue('IP:')} ${chalk.green(scannedip)}`);
+                                    } else if (output.success == true) {
+                                        console.log(`${logSymbols.success} ${chalk.blue('Port:')} ${chalk.green(i)} ${chalk.blue('IP:')} ${chalk.green(scannedip)}`);
+                                    }
+                                }
+                            }
+                        )
+                    }
+                } else if (argv.options == 'r' && !argv.rangeFN || !argv.rangeSN) {
+                    console.log(`${logSymbols.warning} ${chalk.yellow('Please either provide the first, last or both range numbers')}`);
+                } else if (argv.options == 'r' && argv.rangeFN <= 0 || argv.rangeSN > 65535) {
+                    console.log(`${logSymbols.warning} ${chalk.yellow('The first range number must be greater than 1 and the second number must be less than 65535')}`);
+                } else {
+                    console.log(`${logSymbols.warning} ${chalk.yellow('Please give a valid scan type')}`)
+                }
+            }
+        }
     })
     .command('publicip', 'returns client public ip', (yargs) => {
         console.log(chalk.green('Searching...'))
-    }, function (argv) {
-            getIP((err, ip) => {
-                if(err) {
-                    throw err;
-                }
-                console.log(chalk.blue(ip));
-            });
+    }, function(argv) {
+        getIP((err, ip) => {
+            if (err) {
+                throw err;
+            }
+            console.log(chalk.blue(ip));
+        });
     })
     .help()
     .argv
